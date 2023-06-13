a850cd4e42fe160475a55b0374fea584dec1d76d
Rakeshfern <rakesh.sethu@fernsoftware.com>
2023-06-13 18:22:50 +0530

    Circle CI Lambda API

--- Login/app.py
+++ Login/app.py
@@ -4,8 +4,8 @@ def lambda_handler(event, context):
    message={'message':'Execution started successfully!'}
    password=json.loads(event["body"])['Password']
    if password =='whatever':
-      return {'statuscode':200,
+      return {'statusCode':200,
       'headers':{'Content-Type':'application/json'},'body':json.dumps(message)}
    else:
-      return {'statuscode':200,
+      return {'statusCode':200,
       'headers':{'Content-Type':'application/json'},'body':json.dumps(message)}
