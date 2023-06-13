d9d60104458f79506a0800a785b1d2f86359b84c
Rakeshfern <rakesh.sethu@fernsoftware.com>
2023-06-13 18:32:54 +0530

    Lambda api

--- Login/app.py
+++ Login/app.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
    password=json.loads(event["body"])['Password']
    if password =='whatever':
       return {'statusCode':200,
-      'headers':{'Content-Type':'application/json'},'body':json.dumps(message)}
+      'headers':{'Content-Type':'application/json'},'body':"true"}
    else:
       return {'statusCode':200,
-      'headers':{'Content-Type':'application/json'},'body':json.dumps(message)}
+      'headers':{'Content-Type':'application/json'},'body':"false"}
